92e53e67060bbe0378b938b96485e7dbfaede880
Erfan Dejband <e.dejband@gmail.com>
2025-10-16 14:53:53 +0800

    remove one layer of network to compare plot in the dvc

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = Sequential([
     Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
     Dense(32, activation='relu'),
-    Dense(16, activation='relu'),
+    # Dense(16, activation='relu'),
     Dense(1, activation='sigmoid') # Output layer for binary classification
 ])
 
